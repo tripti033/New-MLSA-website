b10a709f396a850654dd0fc6bbcc8273c180e770
Aditi <aditi1042003@gmail.com>
2022-12-29 23:30:02 +0530

    nav and social media icons

--- preloader.js
+++ preloader.js
@@ -9,11 +9,11 @@ config.t2 = 3;
 config.e = Power2.easeOut;
 config.e2 = Power2.easeIn;
 
-config.pageTrans = new TimelineMax({repeat:-1, repeatDelay: 0, yoyo:true});
+config.pageTrans = new TimelineMax({repeat:1, repeatDelay: 0, yoyo:true});
 
 config.pageTrans
 .fromTo('.white',config.t, { x: config.wWidth/2 }, { x: 0, ease: config.e}, "f")
-.fromTo('.grey',config.t, { x: -config.wWidth/2 }, { x: 0, ease: config.e}, "f")
+.fromTo('.grey',config.t, { x: -10-config.wWidth/2 }, { x: 0, ease: config.e}, "f")
 .fromTo('.black',config.t, { y: -config.wHeight }, { y: 0, ease: config.e}, "f")
 .fromTo('.gold',config.t, { y: config.wHeight }, { y: 0, ease: config.e}, "f")
 .fromTo('.grey', config.t2, { y: 0 }, { y: -config.wHeight/2, ease: config.e2}, "f+=.8")
